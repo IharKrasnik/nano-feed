efe5e463b0b708f5549bca97110d57e68cf3ca40
IharKrasnik <ihar.krasnik@gmail.com>
2022-12-29 23:47:17 +0400

    1 file modified

--- src/lib/guards/auth.server.js
+++ src/lib/guards/auth.server.js
@@ -17,40 +17,9 @@ export default async ({ url, cookies }) => {
 				console.log('err', err);
 			}
 
-			// throw redirect(302, '/');
+			throw redirect(302, '/');
 		}
-		console.log('url', url);
-		throw redirect(302, '/_redirect?url=' + url.searchParams.get('otp') + url.href);
 	}
 
 	return {};
 };
-
-// import apiServerSide from '$lib/apiServerSide';
-// import { redirect } from '@sveltejs/kit';
-
-// export default async ({ url, cookies }) => {
-// 	if (!url.href.includes('_redirect')) {
-// 		let otp = new URL(url.href).searchParams.get('otp');
-
-// 		if (otp) {
-// 			let api = apiServerSide({});
-
-// 			try {
-// 				const authData = await api.get('users/current-by-otp', { otp });
-// 				const { accessToken } = authData;
-
-// 				cookies.set('access_token', accessToken);
-// 			} catch (err) {
-// 				console.log('err', err);
-// 			}
-
-// 			throw redirect(302, '/');
-// 		}
-// 		console.log('url', url);
-
-// 		throw redirect(302, '/_redirect?url=' + url);
-// 	}
-
-// 	return {};
-// };
