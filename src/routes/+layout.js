96c6d2aefb36aa3673115297d41902d6546728bf
IharKrasnik <ihar.krasnik@gmail.com>
2022-12-29 20:38:18 +0400

    prerender false

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,5 @@
 import authClientGuard from '$lib/guards/auth.client';
+export const prerender = false;
 
 export function load({ url, params, session }) {
 	return authClientGuard({ url, params, session });
