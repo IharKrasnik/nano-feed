90684becbc55dd36429d6cca9a18fe3d5ce62b08
IharKrasnik <ihar.krasnik@gmail.com>
2022-12-29 20:29:46 +0400

    debug url

--- src/lib/guards/auth.server.js
+++ src/lib/guards/auth.server.js
@@ -2,20 +2,21 @@ import apiServerSide from '$lib/apiServerSide';
 import { redirect } from '@sveltejs/kit';
 
 export default async ({ url, cookies }) => {
-	let otp = new URL(url.href).searchParams.get('otp');
+	if (!url.href.includes('_redirect')) {
+		let otp = new URL(url.href).searchParams.get('otp');
 
-	if (otp) {
-		let api = apiServerSide({});
+		if (otp) {
+			let api = apiServerSide({});
 
-		try {
-			const authData = await api.get('users/current-by-otp', { otp });
-			const { accessToken } = authData;
-			cookies.set('access_token', accessToken);
-		} catch (err) {
-			console.log('err', err);
+			try {
+				const authData = await api.get('users/current-by-otp', { otp });
+				const { accessToken } = authData;
+				cookies.set('access_token', accessToken);
+			} catch (err) {
+				console.log('err', err);
+			}
 		}
-
-		throw redirect(302, '/');
+		throw redirect(302, '/_redirect?url=' + url.href);
 	}
 
 	return {};
