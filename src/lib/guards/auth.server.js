ec1297404bdc7cd072d9b2da4f2c7f735dec16f5
IharKrasnik <ihar.krasnik@gmail.com>
2022-12-29 14:25:37 +0400

    test commit

--- src/lib/guards/auth.server.js
+++ src/lib/guards/auth.server.js
@@ -8,6 +8,7 @@ currentUser.subscribe((value) => (user = value));
 
 export default async ({ url, cookies }) => {
 	throw redirect(302, '/test_url' + url);
+
 	let otp = new URL(url).searchParams.get('otp');
 
 	if (otp) {
