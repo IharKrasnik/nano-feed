c857af5333ec049004aa4cc7c9dc80fd53f15be6
IharKrasnik <ihar.krasnik@gmail.com>
2023-01-15 21:12:47 +0400

    use cdn version for contentwindow script

--- src/routes/[[username]]/embed/+page.svelte
+++ src/routes/[[username]]/embed/+page.svelte
@@ -29,7 +29,7 @@
 </script>
 
 <svelte:head>
-  <script src="https://raw.githubusercontent.com/davidjbradshaw/iframe-resizer/master/js/iframeResizer.contentWindow.min.js" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.contentWindow.min.js" integrity="sha512-14SY6teTzhrLWeL55Q4uCyxr6GQOxF3pEoMxo2mBxXwPRikdMtzKMYWy2B5Lqjr6PHHoGOxZgPaxUYKQrSmu0A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </svelte:head>
 
 <div class="container mx-auto">
