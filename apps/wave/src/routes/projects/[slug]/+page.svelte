ab2e78a3590013d637dca998545ae4b111d1a0ef
Igor Krasnik <ihar.krasnik@gmail.com>
2023-02-14 01:37:25 +0400

    trying to fix loading on prod

--- apps/wave/src/routes/projects/[slug]/+page.svelte
+++ apps/wave/src/routes/projects/[slug]/+page.svelte
@@ -49,38 +49,40 @@
     if (projects?.length && slug) {
       selectedProject = $allProjects.find(p => p.slug === slug);
       
-      stats = await get(`waveProjects/${selectedProject._id}/stats`, {
-        timeframe: selectedTimeframe,
-      });
-      
-      maxViewsCount = stats.pageStats[0].count;
-      maxReferralCount = stats.referralStats[0].count;
-
-      let dateFrom;
-      let unitToAdd;
-      let dateLabelFormat = 'yyyy-MM-DD';
-
-      if (selectedTimeframe === '7_days') {
-        dateFrom = moment().subtract(7, 'days');
-        unitToAdd = 'day';
-        dateLabelFormat = 'yyyy-MM-DD';
-      } else if (selectedTimeframe === '24_hours') {
-        dateFrom = moment().subtract(24, 'hours');
-        unitToAdd = 'hour';
-        dateLabelFormat = 'DD-HH';
-      }
-
-      let now = moment().add(1, 'hour');
-
-      userChartData = {};
-      viewChartData = {};
-
-      while (dateFrom < now) {
-        let dateLabel = dateFrom.format(dateLabelFormat);
+      if (selectedProject.isActive) {
+        stats = await get(`waveProjects/${selectedProject._id}/stats`, {
+          timeframe: selectedTimeframe,
+        });
         
-        userChartData[dateLabel] = stats.userStats.find(s => s.date === dateLabel)?.count || 0;
-        viewChartData[dateLabel] = stats.viewStats.find(s => s.date === dateLabel)?.count || 0;
-        dateFrom.add(1, unitToAdd);
+        maxViewsCount = stats.pageStats[0].count;
+        maxReferralCount = stats.referralStats[0].count;
+
+        let dateFrom;
+        let unitToAdd;
+        let dateLabelFormat = 'yyyy-MM-DD';
+
+        if (selectedTimeframe === '7_days') {
+          dateFrom = moment().subtract(7, 'days');
+          unitToAdd = 'day';
+          dateLabelFormat = 'yyyy-MM-DD';
+        } else if (selectedTimeframe === '24_hours') {
+          dateFrom = moment().subtract(24, 'hours');
+          unitToAdd = 'hour';
+          dateLabelFormat = 'DD-HH';
+        }
+
+        let now = moment().add(1, 'hour');
+
+        userChartData = {};
+        viewChartData = {};
+
+        while (dateFrom < now) {
+          let dateLabel = dateFrom.format(dateLabelFormat);
+          
+          userChartData[dateLabel] = stats.userStats.find(s => s.date === dateLabel)?.count || 0;
+          viewChartData[dateLabel] = stats.viewStats.find(s => s.date === dateLabel)?.count || 0;
+          dateFrom.add(1, unitToAdd);
+        }
       }
     }
 
