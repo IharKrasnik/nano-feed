898fb6a28ff442b759659b72b686cbd1dc2c62ed
Igor Krasnik <ihar.krasnik@gmail.com>
2023-02-15 15:59:28 +0400

    Screen full when theres no stream

--- apps/page/src/lib/components/site-preview.svelte
+++ apps/page/src/lib/components/site-preview.svelte
@@ -51,8 +51,11 @@
     <button class="mt-2 cursor-pointer" on:click={onButtonClick}>{page.callToAction}</button>
   </div>
 
-  <div class="_root">
-    <div class="_content {page.demoUrl ? 'flex-col sm:flex-row justify-between py-32 mb-16' : 'flex-column items-center h-screen'}">
+  <div class="_root" >
+    <div 
+      class="_content {page.demoUrl ? 'flex-col sm:flex-row justify-between py-32 mb-16' : 'flex-column items-center h-screen'}"
+      class:h-screen={!page.streamSlug}
+      >
       <div class="{page.demoUrl ? 'text-left max-w-[500px]' : 'flex flex-col items-center max-w-[800px] mx-auto'}">
         <h1 class="_title" style="color: {page.bgColor}">{page.title}</h1> 
         
