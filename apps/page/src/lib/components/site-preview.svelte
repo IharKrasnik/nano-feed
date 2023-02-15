ca020c2a1f7459cd1790c3af1dd414272d7f1b77
Igor Krasnik <ihar.krasnik@gmail.com>
2023-02-15 14:28:57 +0400

    Responsive view for page

--- apps/page/src/lib/components/site-preview.svelte
+++ apps/page/src/lib/components/site-preview.svelte
@@ -42,28 +42,100 @@
 
 </script>
 
-<div class="_header flex justify-between items-center">
-  <div class="_logo">
-    {page.logo || ''} {page.name}
+<div class="p-8 md:p-0">
+  <div class="_header flex justify-between items-center">
+    <div class="_logo">
+      {page.logo || ''} {page.name}
+    </div>
+
+    <button class="mt-2 cursor-pointer" on:click={onButtonClick}>{page.callToAction}</button>
   </div>
 
-  <button class="mt-2 cursor-pointer" on:click={onButtonClick}>{page.callToAction}</button>
+  <div class="_root">
+    <div class="_content {page.demoUrl ? 'flex-col sm:flex-row justify-between py-32 mb-16' : 'flex-column items-center h-screen'}">
+      <div class="{page.demoUrl ? 'text-left max-w-[500px]' : 'flex flex-col items-center max-w-[800px] mx-auto'}">
+        <h1 class="_title" style="color: {page.bgColor}">{page.title}</h1> 
+        
+        {#if page.subtitle}
+          <h2 class="_subtitle">{page.subtitle}</h2>
+        {/if}
+        
+        <div class="_input_container flex items-center md:w-[392px]">
+          <form class="w-full" on:submit|preventDefault="{submitEmail}">
+
+            {#if !isSubmitted}
+              <input class="_input w-full" placeholder="Your Email" type="email" required bind:this={inputEl} bind:value={email} disabled={isSubmitted}/>
+              <button type="submit" class="_input_button">{page.callToAction}</button>
+            {:else}
+              <div style="color: #000">
+                💥 Thank you!
+              </div>
+
+              {#if page.actionUrl}
+                <div class="mt-8 opacity-70">
+                  Redirecting...
+                </div>
+              {/if}
+            {/if}
+          </form>
+        </div>
+      </div>
+
+      {#if page.demoUrl}
+      <div class="w-full md:w-[600px] mt-8 md:mt-0 md:p-8 md:pr-0">
+        {#if page.demoUrl.includes('loom.com')}
+          <iframe
+            style="width: 100%; aspect-ratio: 536/300;"
+            src={page.demoUrl.replace('share/', 'embed/')}
+          />  
+        {:else}
+          <img src="{page.demoUrl}" />
+        {/if}
+      </div>
+      {/if}
+    </div>
+
+  </div>
 </div>
 
-<div class="_root">
-  <div class="_content {page.demoUrl ? 'flex-row justify-between py-32 mb-16' : 'flex-column items-center h-screen'}">
-    <div class="{page.demoUrl ? 'text-left max-w-[500px]' : 'flex flex-col items-center max-w-[800px] mx-auto'}">
-      <h1 class="_title" style="color: {page.bgColor}">{page.title}</h1> 
-      
-      {#if page.subtitle}
-        <h2 class="_subtitle">{page.subtitle}</h2>
+
+  {#if page.streamSlug}
+    <div class="mt-[-50px]">
+      <h1 class="w-full text-center font-bold text-2xl opacity-70" style="font-family: Inter;">We Build In Public</h1>
+      <h1 class="w-full text-center text-lg opacity-70" style="font-family: Inter;">Follow our journey in social network and blogs.</h1>
+      {#if page.links}
+        <div class="flex justify-center w-full">
+          {#if page.links.twitter}
+          <div class="w-[35px] h-[35px] m-2">
+            <a href="{page.links.twitter}" class="scale-110" target="_blank">
+              <TwitterIcon></TwitterIcon>
+            </a>
+          </div>
+          {/if}
+          {#if page.links.linkedin}
+          <div class="w-[35px] h-[35px] m-2">
+            <a href="{page.links.linkedin}" target="_blank">
+              <LinkedInIcon></LinkedInIcon>
+            </a>
+          </div>
+          {/if}
+        </div>
       {/if}
-      
-      <div class="_input_container flex items-center">
+    </div>
+
+
+    <iframe id="iframeResize" on:load={resize} class="w-full" src="{STREAM_URL}/{page.streamSlug}/embed?theme=light"></iframe>
+
+    <div class="w-full p-4 md:p-32 text-center bg-[#fafafa]">
+      <div class="text-xl mb-8">
+        {page.title}
+      </div>
+
+      <div class="_input_container flex items-center mx-auto md:w-[392px]">
         <form class="w-full" on:submit|preventDefault="{submitEmail}">
 
           {#if !isSubmitted}
-            <input class="_input w-full" placeholder="Your Email" type="email" required bind:this={inputEl} bind:value={email} disabled={isSubmitted}/>
+            <input class="_input w-full" placeholder="Your Email" type="email" required bind:value={email} disabled={isSubmitted}/>
             <button type="submit" class="_input_button">{page.callToAction}</button>
           {:else}
             <div style="color: #000">
@@ -80,77 +152,8 @@
       </div>
     </div>
 
-    {#if page.demoUrl}
-    <div class="w-[600px] p-8 pr-0">
-      {#if page.demoUrl.includes('loom.com')}
-        <iframe
-          style="width: 100%; aspect-ratio: 536/300;"
-          src={page.demoUrl.replace('share/', 'embed/')}
-        />  
-      {:else}
-        <img src="{page.demoUrl}" />
-      {/if}
-    </div>
-    {/if}
-  </div>
+  {/if}
 
-</div>
-
-
-{#if page.streamSlug}
-  <div class="mt-[-50px]">
-    <h1 class="w-full text-center font-bold text-2xl opacity-70" style="font-family: Inter;">We Build In Public</h1>
-    <h1 class="w-full text-center text-lg opacity-70" style="font-family: Inter;">Follow our journey in social network and blogs.</h1>
-    {#if page.links}
-      <div class="flex justify-center w-full">
-        {#if page.links.twitter}
-        <div class="w-[35px] h-[35px] m-2">
-          <a href="{page.links.twitter}" class="scale-110" target="_blank">
-            <TwitterIcon></TwitterIcon>
-          </a>
-        </div>
-        {/if}
-        {#if page.links.linkedin}
-        <div class="w-[35px] h-[35px] m-2">
-          <a href="{page.links.linkedin}" target="_blank">
-            <LinkedInIcon></LinkedInIcon>
-          </a>
-        </div>
-        {/if}
-      </div>
-    {/if}
-  </div>
-
-
-  <iframe id="iframeResize" on:load={resize} class="w-full" src="{STREAM_URL}/{page.streamSlug}/embed?theme=light"></iframe>
-
-  <div class="w-full p-32 text-center bg-[#fafafa]">
-    <div class="text-xl mb-8">
-      {page.title}
-    </div>
-
-    <div class="_input_container flex items-center mx-auto">
-      <form class="w-full" on:submit|preventDefault="{submitEmail}">
-
-        {#if !isSubmitted}
-          <input class="_input w-full" placeholder="Your Email" type="email" required bind:value={email} disabled={isSubmitted}/>
-          <button type="submit" class="_input_button">{page.callToAction}</button>
-        {:else}
-          <div style="color: #000">
-            💥 Thank you!
-          </div>
-
-          {#if page.actionUrl}
-            <div class="mt-8 opacity-70">
-              Redirecting...
-            </div>
-          {/if}
-        {/if}
-      </form>
-    </div>
-  </div>
-
-{/if}
 
 <style>
   ._root {
@@ -195,7 +198,6 @@
 
   ._input_container {
     position: relative;
-    width: 392px;
   }
 
   ._input {
