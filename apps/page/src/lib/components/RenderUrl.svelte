b5c1633eb349542719da2245962a243f9eceb428
Igor Krasnik <ihar.krasnik@gmail.com>
2023-02-16 19:23:11 +0400

    attach youtube

--- apps/page/src/lib/components/RenderUrl.svelte
+++ apps/page/src/lib/components/RenderUrl.svelte
@@ -19,6 +19,16 @@
           style="width: 100%; aspect-ratio: 536/300;"
           src={url.replace('share/', 'embed/')}
         />  
+      {:else if url.includes('youtube.com') || url.includes('youtu.be')}
+        <iframe
+          class="w-full mt-2"
+          style="aspect-ratio: 536/300;"
+          src="https://www.youtube.com/embed/{new URL(url).searchParams.get('v')}"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
       {:else}
         {#if url.includes('.mp4') || url.includes('mov')}
           <video class="max-w-[600px] mx-auto {imgClass}" autoplay muted loop src={url}></video>
