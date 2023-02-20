b3d7ef9f194b87429ceba0efd4c94b2124b048f7
Igor Krasnik <ihar.krasnik@gmail.com>
2023-02-20 13:50:23 +0400

    Set subproject id

--- apps/page/src/routes/[pageSlug]/+page.svelte
+++ apps/page/src/routes/[pageSlug]/+page.svelte
@@ -1,27 +1,21 @@
 <script>
-  import { browser } from '$app/environment';
-  import { onMount } from 'svelte';
-  import { page } from '$app/stores';
-  import { get } from 'lib/api';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import { get } from 'lib/api';
 
-  import SitePreview from '$lib/components/site-preview.svelte';
+	import SitePreview from '$lib/components/site-preview.svelte';
 
-  let slug;
+	let slug;
 
-  let currentPage = $page.data.page;
+	let currentPage = $page.data.page;
 
-  if (browser) {
-    window.WAVE_SESSION_KEY = $page.params.pageSlug;
-    slug = $page.params.pageSlug;
-  }
-
-
+	if (browser && currentPage) {
+		window.WAVE_SUBPROJECT_ID = currentPage._id;
+		slug = $page.params.pageSlug;
+	}
 </script>
 
-
 {#if page}
-  <SitePreview
-    bind:page={currentPage}
-  >
-  </SitePreview>
+	<SitePreview bind:page={currentPage} />
 {/if}
