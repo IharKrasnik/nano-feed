8f50e350387e28e01f37a21cbc8374684fc72fd0
Igor Krasnik <ihar.krasnik@gmail.com>
2023-02-28 14:40:22 +0400

    Store fingerprint in cookies and window

--- apps/wave/tracker/src/tracker.js
+++ apps/wave/tracker/src/tracker.js
@@ -42,6 +42,28 @@ const calcIdentificator = () =>
 		});
 	});
 
+function setCookie(c_name, value, exdays) {
+	var exdate = new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value = escape(value) + (exdays == null ? '' : '; expires=' + exdate.toUTCString());
+	document.cookie = c_name + '=' + c_value;
+}
+
+function getCookie(c_name) {
+	var i,
+		x,
+		y,
+		ARRcookies = document.cookie.split(';');
+	for (i = 0; i < ARRcookies.length; i++) {
+		x = ARRcookies[i].substr(0, ARRcookies[i].indexOf('='));
+		y = ARRcookies[i].substr(ARRcookies[i].indexOf('=') + 1);
+		x = x.replace(/^\s+|\s+$/g, '');
+		if (x == c_name) {
+			return unescape(y);
+		}
+	}
+}
+
 const init =
 	(initialUserData, handlers = defaultHandlers) =>
 	() => {
@@ -50,6 +72,8 @@ const init =
 		return calcIdentificator()
 			.then((visitorId) => {
 				userData.visitorId = visitorId;
+				setCookie('wave_fingerprint', visitorId);
+				window.WAVE_FINGERPRINT = visitorId;
 
 				return fetch('https://api.ipify.org')
 					.then((res) => res.text())
