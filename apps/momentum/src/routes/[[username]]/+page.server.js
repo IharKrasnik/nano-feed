88849e50b130aab803e7c08e32bb6a88a214dbec
Igor Krasnik <ihar.krasnik@gmail.com>
2023-08-10 18:36:07 +0200

    Fix momentum meta tags

--- apps/momentum/src/routes/[[username]]/+page.server.js
+++ apps/momentum/src/routes/[[username]]/+page.server.js
@@ -4,29 +4,32 @@ import { get } from 'lib/api';
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
 	const { username } = params;
-	let desciption = 'Momentum is a tool to build in public and grow audience early.';
-
+	let description = 'Momentum is a tool to build in public and grow audience early.';
+	let image =
+		'https://assets.website-files.com/636cf54cf20a6ac090f7deb0/6400e2d9b05733208d9b29b4_open-graph%20(3).png';
 	if (username && username !== 'explore') {
 		if (username?.startsWith('@')) {
 			let creator = await get(`creators/${username.replace('@', '')}`);
 
 			return {
 				ogTitle: `${creator.fullName} — Momentum Stream`,
-				ogDescription: desciption
+				ogDescription: creator.description || description,
+				ogImage: image
 			};
 		} else {
 			let project = await get(`projects/${username}`);
 
 			return {
 				ogTitle: `${project.title} — Momentum Stream`,
-				ogDescription: desciption
+				ogDescription: project.description || description,
+				ogImage: project.bannerUrl || image
 			};
 		}
 	}
-	console.log('wtf', desciption);
 
 	return {
 		ogTitle: 'Momentum',
-		ogDescription: desciption
+		ogDescription: description,
+		ogImage: image
 	};
 }
