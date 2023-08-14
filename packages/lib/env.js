c1b0913f55fedc0afa34cf90de70a5b37d60d41d
Igor Krasnik <ihar.krasnik@gmail.com>
2023-08-14 23:04:10 +0200

    env google

--- packages/lib/env.js
+++ packages/lib/env.js
@@ -4,17 +4,17 @@ import { browser } from '$app/environment';
 
 //
 
-export const API_URL = dev
-	? 'https://ship-627ed08f489a970013572f5d.paralect.net'
-	: 'https://ship-prod-627ed08f489a970013572f5d.paralect.net';
+// export const API_URL = dev
+// 	? 'https://ship-627ed08f489a970013572f5d.paralect.net'
+// 	: 'https://ship-prod-627ed08f489a970013572f5d.paralect.net';
 
-// export const API_URL = 'https://ship-prod-627ed08f489a970013572f5d.paralect.net';
+export const API_URL = 'https://ship-prod-627ed08f489a970013572f5d.paralect.net';
 
 export const WAVE_URL = dev ? 'https://wave.mmntm.build' : 'https://wave.mmntm.build';
 
 export const PAGE_URL = dev ? 'http://localhost:5176' : 'https://mmntm.me';
 
-export const POST_URL = dev ? 'http://localhost:5179' : 'https://mmntm.live';
+export const POST_URL = dev ? 'http://localhost:5179' : 'https://mmntm.blog';
 
 export const STREAM_URL = dev
 	? 'http://localhost:5173'
@@ -24,6 +24,10 @@ export const STREAM_URL = dev
 export let GOOGLE_LOGIN_URL = browser
 	? `${API_URL}/auth/google/url?redirect_to=${window.document.location.href}`
 	: null;
+
+export let getGoogleLoginUrl = () => {
+	return browser ? `${API_URL}/auth/google/url?redirect_to=${window.document.location.href}` : null;
+};
 
 export const LINKEDIN_LOGIN_URL = browser
 	? `${API_URL}/auth/linkedin/url?redirect_to=${window.document.location.href}`
