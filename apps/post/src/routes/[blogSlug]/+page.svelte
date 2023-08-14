3633c402b812ad349edca7a20d99128408085bc3
Igor Krasnik <ihar.krasnik@gmail.com>
2023-08-14 02:46:01 +0200

    Fix post

--- apps/post/src/routes/[blogSlug]/+page.svelte
+++ apps/post/src/routes/[blogSlug]/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import { page } from '$app/stores';
+	import PostPreview from '$lib/components/PostPreview.svelte';
 	import BlogPreview from '$lib/components/BlogPreview.svelte';
 </script>
 
-{#if $page.data.blog?.posts?.length}
-	<BlogPreview blog={$page.data.blog} posts={$page.data.blog.posts} />
+{#if $page.data.post}
+	<PostPreview isEdit={false} post={$page.data.post} />
+{:else if $page.data.blog}
+	<BlogPreview blog={$page.data.blog} posts={$page.data.blog?.posts || []} />
 {/if}
