f4f8768bec32c9a4b2fc1ce336417b706d171785
Igor Krasnik <ihar.krasnik@gmail.com>
2023-08-17 17:30:44 +0200

    Fix env

--- packages/lib/env.js
+++ packages/lib/env.js
@@ -4,17 +4,15 @@ import { browser } from '$app/environment';
 
 //
 
-export const API_URL = dev
-	? 'https://ship-627ed08f489a970013572f5d.paralect.net'
-	: 'https://momentum-api.paralect.com';
+// export const API_URL = dev
+// 	? 'https://ship-627ed08f489a970013572f5d.paralect.net'
+// 	: 'https://momentum-api.paralect.com';
 
-//1
-
-// export const API_URL = 'https://ship-prod-627ed08f489a970013572f5d.paralect.net';
+export const API_URL = 'https://momentum-api.paralect.com';
 
 export const WAVE_URL = dev ? 'https://wave.mmntm.build' : 'https://wave.mmntm.build';
 
-export const BRAND_URL = dev ? 'http://localhost:5177' : 'https://brand.mmntm.build';
+export const BRAND_URL = dev ? 'http://localhost:5173' : 'https://brand.mmntm.build';
 
 export const PAGE_URL = dev ? 'http://localhost:5176' : 'https://mmntm.me';
 
