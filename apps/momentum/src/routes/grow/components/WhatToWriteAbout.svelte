88ef395428ec8ff8f4763ec8f3d683f933f3d1db
Igor Krasnik <ihar.krasnik@gmail.com>
2023-08-24 00:46:37 +0200

    Dont miss aday

--- apps/momentum/src/routes/grow/components/WhatToWriteAbout.svelte
+++ apps/momentum/src/routes/grow/components/WhatToWriteAbout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import _ from 'lodash';
+	import { fade } from 'svelte/transition';
 	import FeedItem from '$lib/components/FeedItem.svelte';
 	import currentUser, { isLoading as isUserLoading } from 'lib/stores/currentUser';
+	import RenderUrl from 'lib/components/RenderUrl.svelte';
 
 	export let onSelected = ({ text }) => {};
 
@@ -16,33 +18,68 @@
 			<div
 				on:click={() => (activeContentType = contentType)}
 				class="cursor-pointer p-4 rounded-lg mb-2 w-[250px] transition"
-				class:bg-zinc-700={activeContentType.name === contentType.name}
+				class:bg-zinc-700={activeContentType?.name === contentType.name}
 				style="border: 1px rgba(255, 255, 255, .3) solid;"
 			>
 				{contentType.name}
 			</div>
 		{/each}
 
-		<div class="mt-16 p-4 rounded-xl bg-yellow-800 hover:bg-yellow-700 transition cursor-pointer">
+		<div
+			class="mt-16 p-4 rounded-xl transition cursor-pointer"
+			class:hover:bg-yellow-800={activeContentType}
+			class:bg-yellow-900={activeContentType}
+			class:bg-green-800={!activeContentType}
+			on:click={() => (activeContentType = null)}
+		>
 			🧠 PRO TIP: Don't miss a day
 		</div>
 	</div>
 
-	<div class="p-8 bg-zinc-900">
-		<div class="mb-8">
-			<h2>{activeContentType.name}</h2>
-			<h2 class="mt-4">Questions</h2>
-			{#each activeContentType.questions as question}
-				<h3>• {question}</h3>
+	{#if activeContentType}
+		<div class="p-8 bg-zinc-900">
+			<div class="mb-8">
+				<h2>{activeContentType.name}</h2>
+				<h2 class="mt-4">Questions</h2>
+				{#each activeContentType.questions as question}
+					<h3>• {question}</h3>
+				{/each}
+			</div>
+			<h2 class="mb-2">Examples / Templates</h2>
+			{#each activeContentType.examples as example}
+				<FeedItem
+					hideLikes={true}
+					feedItem={{ content: example.text, creators: [$currentUser], publishedOn: new Date() }}
+					onClick={() => onSelected(example)}
+				/>
 			{/each}
 		</div>
-		<h2 class="mb-2">Examples / Templates</h2>
-		{#each activeContentType.examples as example}
-			<FeedItem
-				hideLikes={true}
-				feedItem={{ content: example.text, creators: [$currentUser], publishedOn: new Date() }}
-				onClick={() => onSelected(example)}
-			/>
-		{/each}
-	</div>
+	{:else}
+		<div class="p-8 bg-zinc-900 w-full">
+			<div in:fade={{}} class=" w-full">
+				<h2 class="mb-8">🦸 PRO: Don't miss a day</h2>
+
+				<h2>Let the true magic happen</h2>
+				<RenderUrl
+					class="w-full"
+					url="https://www.youtube.com/watch?v=W2SExKkVx7M&ab_channel=IgorKrasnik"
+				/>
+
+				<h2 class="mt-8 mb-2">Make it your "Homework for life"</h2>
+				<h3 class="opacity-80 mt-2">
+					Matthew Dicks is an author of "Storyworthy" — the great practical book on storytelling
+				</h3>
+
+				<RenderUrl url="https://www.youtube.com/watch?v=x7p329Z8MD0" />
+
+				<h2 class="mt-8 mb-2">Develop an "atomic habit"</h2>
+				<h3>Atomic habits is a "self-help" bestseller book</h3>
+				<RenderUrl
+					url="https://www.youtube.com/watch?v=PZ7lDrwYdZc&pp=ygUNYXRvbWljIGhhYmJpdA%3D%3D"
+				/>
+
+				<h3>Should we coin a "Nano" habbit? 🤔</h3>
+			</div>
+		</div>
+	{/if}
 </div>
