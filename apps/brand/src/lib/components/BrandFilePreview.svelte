45ab9bbddf36e40c782f8919a5cfb20bfd12eb7a
Igor Krasnik <ihar.krasnik@gmail.com>
2023-08-24 18:27:00 +0200

    Brand mobile

--- apps/brand/src/lib/components/BrandFilePreview.svelte
+++ apps/brand/src/lib/components/BrandFilePreview.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { browser } from '$app/environment';
 	import _ from 'lodash';
 
 	export let file;
@@ -12,12 +13,21 @@
 	}
 
 	import ImagePreview from './../../routes/doc.pdf/Image.svelte';
+
+	let isMobile;
+
+	if (browser && window.innerWidth) {
+		isMobile = window.innerWidth < 600;
+	}
 </script>
 
 <div
 	class="transition"
-	style="transform: scale(0.25); transform-origin: top left; max-height: {(file.size?.height ||
-		1080) / 4}px;"
+	style="transform: scale({isMobile
+		? '0.25'
+		: '0.5'}); transform-origin: top left; max-height: {(file.size?.height || 1080) / isMobile
+		? 4
+		: 2}px;"
 >
 	{#key file}
 		<ImagePreview isPreview={true} page={file} />
