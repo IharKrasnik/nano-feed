7441d10a2e910c233d7a01320a9fbe71cf9edcfb
Igor Krasnik <ihar.krasnik@gmail.com>
2023-09-07 13:32:45 +0200

    fs garbage

--- apps/fs/src/routes/+layout.server.js
+++
@@ -1,7 +0,0 @@
-import authServerGuard from 'lib/guards/auth.server';
-
-export async function load({ url, params, session, cookies }) {
-	let authData = await authServerGuard({ url, params, session, cookies }, 'Momentum Engage');
-
-	return { ...authData };
-}
