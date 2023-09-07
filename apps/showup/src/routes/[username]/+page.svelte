61bc1f75b91a8d073205588eb3773b28f04f259f
Igor Krasnik <ihar.krasnik@gmail.com>
2023-09-07 19:07:38 +0200

    Showup nice profiles

--- apps/showup/src/routes/[username]/+page.svelte
+++ apps/showup/src/routes/[username]/+page.svelte
@@ -17,6 +17,7 @@
 	import EmojiPicker from 'lib/components/EmojiPicker.svelte';
 	import Twitter from 'lib/icons/twitter.svelte';
 	import JournalFeedItem from '$lib/components/JournalFeedItem.svelte';
+	import MomentumLogo from 'lib/icons/momentum.svelte';
 	import AchievedBadge from '$lib/components/AchievedBadge.svelte';
 
 	let streamSlug = 'show-up-daily';
@@ -47,33 +48,70 @@
 	let date = moment();
 	let title = moment(date).format('dddd, MMM DD');
 
-	getCreator();
-
-	getFeed();
+	$: if ($page.params.username) {
+		getCreator();
+		getFeed();
+	}
 </script>
 
 {#if creator}
 	<div class="container max-w-[700px] mx-auto">
 		<div class="px-4 sm:px-0 py-4">
 			<div class="my-8 mb-16">
-				<a class="flex flex-col items-center">
+				<div class=" flex flex-col items-center">
+					{#if creator.bannerUrl}
+						<img class="mb-[-50px] rounded-xl" src={creator.bannerUrl} />
+					{/if}
 					<Avatar size="75px" user={creator} />
-					<h3 class="text-xl opacity-80 my-4">{creator.fullName}</h3>
-				</a>
+					<h3 class="text-xl opacity-80 mt-4 mb-2 font-bold">{creator.fullName}</h3>
+					{#if creator.description}
+						<div class="opacity-80 my-2 text-lg mb-4">{creator.description}</div>
+					{/if}
+
+					<div class="flex gap-2">
+						{#if creator.twitterData}
+							<a
+								class="flex bg-[#e4e4e4] items-center my-2 p-2 rounded-xl bg-[#]"
+								target="_blank"
+								href="https://twitter.com/{creator.twitterData.username}"
+							>
+								<div class="w-[20px] mr-1">
+									<Twitter />
+								</div>
+								<div class="opacity-80">
+									{creator.twitterData.username}
+								</div>
+							</a>
+						{/if}
+
+						<a
+							class="flex bg-[#e4e4e4] items-center my-2 p-2 rounded-xl bg-[#]"
+							target="_blank"
+							href="https://feed.mmntm.build/@{creator.username}"
+						>
+							<div class="w-[20px] mr-1">
+								<MomentumLogo />
+							</div>
+							<div class="opacity-80">
+								{creator.username}
+							</div>
+						</a>
+					</div>
+					{#if $currentUser && $currentUser._id === creator._id}
+						<a
+							target="_blank"
+							href="https://feed.mmntm.build/@{creator.username}/edit"
+							class="button mt-2 py-2"
+							style="padding: 8px 16px;">💥 Edit My Profile</a
+						>
+					{/if}
+				</div>
 			</div>
 
 			{#each journalFeed as feedItem (feedItem._id)}
 				<JournalFeedItem {feedItem} />
 			{/each}
 		</div>
-	</div>
-{/if}
-
-{#if !$currentUser && isShown}
-	<div class="w-full flex justify-center mt-16 mb-16" in:fade={{ delay: 300 }}>
-		<a class="button" style="background: none; border: 3px #555 solid; color: #555;" href={'/'}
-			>Publish your journal
-		</a>
 	</div>
 {/if}
 
