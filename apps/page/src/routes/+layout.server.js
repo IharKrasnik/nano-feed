60765e3e11740436e66f43b26a7e11afcecff0c6
Igor Krasnik <ihar.krasnik@gmail.com>
2023-11-10 18:21:33 +0100

    Fix slug

--- apps/page/src/routes/+layout.server.js
+++ apps/page/src/routes/+layout.server.js
@@ -10,17 +10,16 @@ let getDomain = (href) => {
 export async function load({ url, params, session, cookies }) {
 	let currentDomain = getDomain(url.href);
 
-	let pageSlug = currentDomain.includes('localhost')
-		? url.searchParams.get('pageSlug')
-		: currentDomain;
+	let pageSlug =
+		currentDomain.includes('localhost') || currentDomain.includes('ide.momentum.page')
+			? url.searchParams.get('pageSlug')
+			: currentDomain;
 
 	const { subPageSlug } = params;
 
 	let extend = {
 		pageSlug
 	};
-
-	console.log('asdasdasdasdasda', extend);
 
 	if (false || pageSlug) {
 		let page = await get(`pages/${subPageSlug || pageSlug}`, {
