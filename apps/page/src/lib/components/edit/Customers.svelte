fd306e84235c8bc57916c44670ed5876fbe2d701
Igor Krasnik <ihar.krasnik@gmail.com>
2023-11-14 20:24:10 +0100

    Edit customer notes

--- apps/page/src/lib/components/edit/Customers.svelte
+++ apps/page/src/lib/components/edit/Customers.svelte
@@ -1,6 +1,10 @@
 <script>
+	import moment from 'moment';
 	import { get, post, put, del } from 'lib/api';
+	import { fade } from 'svelte/transition';
 	import { countryCodeEmoji } from 'country-code-emoji';
+	import Button from 'lib/components/Button.svelte';
+	import FeatherIcon from 'lib/components/FeatherIcon.svelte';
 
 	export let page;
 
@@ -8,15 +12,72 @@
 
 	let loadCustomers = async () => {
 		let customerResult = await get(`customers`, { pageId: page._id });
-		customers = customerResult.results;
+
+		customers = customerResult.results.map((c) => {
+			c.isCollapsed = true;
+			return c;
+		});
 	};
 
 	loadCustomers();
+
+	let saveCustomer = async (customer) => {
+		let updatedCustomer = await put(`customers/${customer._id}?pageId=${page._id}`, customer);
+
+		customers = customers.map((c) => {
+			if (c._id === updatedCustomer._id) {
+				updatedCustomer.isCollapsed = true;
+				return updatedCustomer;
+			}
+			return c;
+		});
+	};
 </script>
 
 {#each customers as customer}
-	<div class="_section">
-		{countryCodeEmoji(customer?.visitor?.geoData?.country)}
-		{customer.email}
-	</div>
+	{#if customer.isCollapsed}
+		<div
+			class="_section cursor-pointer"
+			on:click={() => {
+				customer.isCollapsed = false;
+			}}
+		>
+			<div class=" flex justify-between items-center">
+				{#if customer.name}
+					<div class="flex items-center">
+						{customer.name}
+						<div class="ml-2 opacity-50">({customer.email})</div>
+					</div>
+				{:else}
+					{customer.email}
+				{/if}
+
+				<div class="flex gap-2 items-center">
+					{#if customer?.visitor?.payload?.deviceType}
+						{#if customer.visitor.payload.deviceType === 'desktop'}
+							<FeatherIcon theme="light" size="15" name="monitor" />
+						{:else}
+							<FeatherIcon theme="light" size="15" name="smartphone" />
+						{/if}
+					{/if}
+					{#if customer?.visitor?.geoData?.country}
+						{countryCodeEmoji(customer?.visitor?.geoData?.country)}
+					{/if}
+				</div>
+			</div>
+			<div class="text-sm mt-2">
+				{moment(customer.createdOn).format('MMM DD')}
+			</div>
+		</div>
+	{:else}
+		<div class="_section flex flex-col gap-2" in:fade={{ duration: 150 }}>
+			<input class="w-full" bind:value={customer.email} disabled />
+			<input class="w-full" placeholder="Name" bind:value={customer.name} />
+			<textarea rows="2" class="w-full" placeholder="Notes..." bind:value={customer.notes} />
+
+			<Button class="_small _primary mt-4 block" onClick={() => saveCustomer(customer)}
+				>💾 Save Customer</Button
+			>
+		</div>
+	{/if}
 {/each}
