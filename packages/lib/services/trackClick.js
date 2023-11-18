3f527071fce65a5cc25d6ffa63c1e2f3ae0c7248
Igor Krasnik <ihar.krasnik@gmail.com>
2023-11-18 15:44:02 +0100

    Track clicks with page

--- packages/lib/services/trackClick.js
+++ packages/lib/services/trackClick.js
@@ -2,6 +2,7 @@ export default ({ sectionId, sectionItemId = null, isFeedItem, linkId, url = nul
 	return window.mwave.sendTrackEvent({
 		type: 'click_custom',
 		payload: {
+			fromUrl: window.location.href,
 			sectionId,
 			sectionItemId,
 			linkId,
