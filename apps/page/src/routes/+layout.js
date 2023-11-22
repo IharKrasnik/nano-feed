48c1958e0027299590e2dedb568864f7e1380a2e
Igor Krasnik <ihar.krasnik@gmail.com>
2023-11-22 16:54:00 +0100

    fix

--- apps/page/src/routes/+layout.js
+++ apps/page/src/routes/+layout.js
@@ -1,54 +1,11 @@
 import authClientGuard from 'lib/guards/auth.client';
-import { redirect } from '@sveltejs/kit';
-
-import { PAGE_URL } from 'lib/env';
-import { get } from 'lib/api';
-
-import getPageMetaTags from 'lib/helpers/getPageMetaTags';
-
-let getDomain = (href) => {
-	let res = /:\/\/([^\/]+)/.exec(href);
-	return (res && res[1]) || href;
-};
 
 export async function load({ url, params, session, cookies, data }) {
-	let currentDomain = getDomain(url.href);
-
-	let pageSlug =
-		currentDomain.includes('localhost') || currentDomain.includes('ide.momentum.page')
-			? url.searchParams.get('pageSlug')
-			: currentDomain;
-
-	const { subPageSlug } = params;
-
 	let extend = {
 		...data
 	};
 
-	// if (pageSlug) {
-	// 	let page = await get(`pages/${subPageSlug || pageSlug}`, {
-	// 		parentPageSlug: subPageSlug ? pageSlug : '',
-	// 		isServer: true
-	// 	});
-
-	// 	let metaTags = getPageMetaTags({ page });
-	// 	//s
-	// 	extend = {
-	// 		page,
-	// 		ogTitle: metaTags.title,
-	// 		ogDescription: metaTags.description,
-	// 		ogImage: metaTags.image
-	// 	};
-	// } else {
-	// 	extend = {
-	// 		ogTitle: `Momentum IDE`,
-	// 		ogDescription: `Grow your startup: launch landing pages, collect waitlist, create, connect with your audience, sell.`,
-	// 		ogImage:
-	// 			'https://ship-app-assets.fra1.digitaloceanspaces.com/stream/rec4sLfwGXzHxLy54/1691926283375-telegram-cloud-document-2-5386494382004252533.jpg'
-	// 	};
-	// }
-
 	let authData = await authClientGuard({ url, params, session }, 'Momentum IDE');
 
-	return { ...authData, ...extend, pageSlug };
+	return { ...authData, ...extend };
 }
