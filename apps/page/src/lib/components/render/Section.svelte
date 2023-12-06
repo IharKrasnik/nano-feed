36c1465684dd035e7a957dd3b653c3dbd861e574
Igor Krasnik <ihar.krasnik@gmail.com>
2023-12-06 20:26:28 +0100

    sectiong

--- apps/page/src/lib/components/render/Section.svelte
+++ apps/page/src/lib/components/render/Section.svelte
@@ -364,7 +364,7 @@
 			{#if !isSkipHeader && (section.title || section.description || section.imageUrl || section.emoji || section.interactiveRenderType)}
 				{#if section.title && (section.items?.length || section.collectionType)}
 					<div
-						class="absolute inset-x-0 top-20 mx-auto h-32 w-[650px] transform-gpu opacity-[15%] blur-[130px] bg-gradient-to-r _from-text-color _to-accent"
+						class="absolute inset-x-0 top-20 mx-auto h-32 w-full sm:w-[650px] transform-gpu opacity-[15%] blur-[130px] bg-gradient-to-r _from-text-color _to-accent"
 					/>
 				{/if}
 				<div
