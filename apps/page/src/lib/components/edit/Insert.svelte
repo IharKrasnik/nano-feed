784633093cf9bac915e0b270f8ad2efc3d7a57ab
Igor Krasnik <ihar.krasnik@gmail.com>
2023-12-16 23:39:40 +0100

    Insert interface

--- apps/page/src/lib/components/edit/Insert.svelte
+++ apps/page/src/lib/components/edit/Insert.svelte
@@ -14,7 +14,9 @@
 </script>
 
 {#if isInsertPopupShown}
-	<div class="_insert absolute left-0 top-[60px] w-[400px] bg-white h-screen p-4">
+	<div
+		class="_insert absolute left-0 top-[60px] w-[400px] bg-white h-screen overflow-y-auto p-4 pb-32"
+	>
 		<div
 			class="absolute z-10 cursor-pointer top-4 right-4"
 			on:click={() => (isInsertPopupShown = false)}
@@ -22,13 +24,13 @@
 			<FeatherIcon class="opacity-50 hover:opacity-100 cursor-pointer" name="x" color="#333333" />
 		</div>
 
-		<div class="flex items-center font-semibold opacity-80 text-sm">
+		<div class="flex items-center font-semibold opacity-80 ">
 			<div
-				class="bg-yellow-600 p-[4px] rounded-lg shadow-md shadow-yellow-600/30 flex items-center justify-center w-[30px] h-[30px] mr-2"
+				class="bg-yellow-600 p-2 rounded-full shadow-md shadow-yellow-600/30 flex items-center justify-center w-[40px] h-[40px] mr-3"
 			>
 				<FeatherIcon name="grid" color="#f6f5f4" />
 			</div>
-			Sections
+			Quick Sections
 		</div>
 
 		<div
@@ -49,16 +51,17 @@
 			}}
 		>
 			Add Empty Section
+			<div class="text-sm opacity-70">Design from scratch</div>
 		</div>
 		<div class="border-bottom py-4 w-full _section">
-			<div class="font-medium opacity-80 mb-2">Embed Anything</div>
-			<FileInput class="w-full" placeholder="Media URL" theme="light" />
+			<div class="font-medium opacity-80 mb-2">Embed File or URL</div>
+			<FileInput isWithIntegrations class="w-full" placeholder="Media URL" theme="light" />
 		</div>
 
-		<div class="flex justify-between mt-7">
-			<div class="flex items-center font-semibold opacity-80 text-sm">
+		<div class="flex justify-between mt-16">
+			<div class="flex items-center font-semibold opacity-80">
 				<div
-					class="bg-green-600 p-[4px] rounded-lg shadow-md shadow-green-600/30 flex items-center justify-center w-[30px] h-[30px] mr-2"
+					class="bg-green-600 p-2 rounded-full shadow-md shadow-green-600/30 flex items-center justify-center w-[40px] h-[40px] mr-3"
 				>
 					<FeatherIcon name="gift" color="#f6f5f4" />
 				</div>
@@ -82,7 +85,12 @@
 								{templateFeedItem.content}
 							</div>
 						{/if}
-						<img class="w-full rounded" src={templateFeedItem.attachments[0].url} />
+						<div class="aspect-og w-full bg-[#f1f1f1]">
+							<img
+								class="aspect-og object-cover w-full rounded"
+								src={templateFeedItem.attachments[0].url}
+							/>
+						</div>
 					</div>
 				{/each}
 			</div>
