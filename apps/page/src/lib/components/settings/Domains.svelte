4038d0bd8ca18ae79d2b8498370d545e933366b7
Igor Krasnik <ihar.krasnik@gmail.com>
2023-12-20 18:08:28 +0100

    Domains adding ui

--- apps/page/src/lib/components/settings/Domains.svelte
+++ apps/page/src/lib/components/settings/Domains.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { get, post } from 'lib/api';
 	import Button from 'lib/components/Button.svelte';
+	import { showSuccessMessage } from 'lib/services/toast';
 
 	export let page;
 
@@ -58,19 +59,92 @@
 					<div class="font-bold">Set the following record on your DNS provider to continue:</div>
 					<div class="mt-2">
 						{#if domain.url.split('.').length === 2}
-							A @ 76.76.21.21
+							A Record @ 76.76.21.21
 
-							<div class="mt-2 text-sm">
-								Read <a href="https://uk.godaddy.com/help/add-an-a-record-19238" target="_blank"
-									>Tutorial</a
+							<div class="flex mt-4">
+								<div>
+									<div class="font-semibold text-xs mb-1">Name</div>
+									<input class="_dark w-[100px]" type="text" disabled value="@" />
+									<button
+										on:click={() => {
+											navigator.clipboard.writeText(domain.url.split('.')[0]);
+											showSuccessMessage('@');
+										}}>copy</button
+									>
+								</div>
+
+								<div class="ml-12">
+									<div class="font-semibold text-xs mb-1">Content</div>
+									<input class="_dark" type="text" disabled value="76.76.21.21" />
+									<button
+										on:click={() => {
+											navigator.clipboard.writeText('76.76.21.21');
+											showSuccessMessage('Copied to clipboard');
+										}}>copy</button
+									>
+								</div>
+							</div>
+							<div>
+								<img
+									class="my-4"
+									src="https://ship-app-assets.fra1.digitaloceanspaces.com/stream/rec4sLfwGXzHxLy54/1703091909820-image.png"
+								/>
+							</div>
+							<div class="mt-4 text-sm">
+								Don't know how to add DNS records? <br />
+
+								<a
+									class="underline"
+									href="https://uk.godaddy.com/help/add-an-a-record-19238"
+									target="_blank"
+								>
+									Read Tutorial</a
 								>
 							</div>
 						{:else}
-							CNAME {domain.url.split('.')[0]} cname.vercel-dns.com.
+							CNAME DNS record
+							<div class="grid grid-cols-2 mt-2">
+								<div class=" ">
+									<div class="font-semibold text-xs mb-1">Name</div>
+									<input class="_dark" type="text" disabled value={domain.url.split('.')[0]} />
+									<button
+										on:click={() => {
+											navigator.clipboard.writeText(domain.url.split('.')[0]);
+											showSuccessMessage('Copied to clipboard');
+										}}>copy</button
+									>
+								</div>
+								<div>
+									<div class="font-semibold text-xs mb-1">Content</div>
+									<input class="_dark" type="text" disabled value="cname.vercel-dns.com." />
+									<button
+										on:click={() => {
+											navigator.clipboard.writeText('cname.vercel-dns.com.');
+											showSuccessMessage('Copied to clipboard');
+										}}>copy</button
+									>
+								</div>
+							</div>
 
-							<div class="mt-2 text-sm">
-								Read <a href="https://uk.godaddy.com/help/add-a-cname-record-19236" target="_blank"
-									>Tutorial</a
+							{#if domain.url.includes('www.')}
+								<img
+									class="my-4"
+									src="https://ship-app-assets.fra1.digitaloceanspaces.com/stream/rec4sLfwGXzHxLy54/1703091998051-image.png"
+								/>
+							{:else}
+								<img
+									class="my-4"
+									src="https://ship-app-assets.fra1.digitaloceanspaces.com/stream/rec4sLfwGXzHxLy54/1703090079838-image.png"
+								/>
+							{/if}
+
+							<div class="mt-4 text-sm">
+								Don't know how to add DNS records? <br />
+
+								<a
+									class="underline"
+									href="https://uk.godaddy.com/help/add-a-cname-record-19236"
+									target="_blank">Read Tutorial</a
 								>
 							</div>
 						{/if}
