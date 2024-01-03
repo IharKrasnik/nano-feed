01ef6efa19f8c1f8b40aa62e40a9d12bf46fe8dc
Igor Krasnik <ihar.krasnik@gmail.com>
2024-01-03 15:13:37 +0100

    Services

--- apps/page-render/src/routes/service-requests/[serviceRequestId]/+page.svelte
+++ apps/page-render/src/routes/service-requests/[serviceRequestId]/+page.svelte
@@ -1,10 +1,6 @@
 <script>
-	import RenderServiceChat from 'lib-render/components/render/ServiceChat.svelte';
+	// import RenderServiceChat from 'lib-render/components/render/ServiceChat.svelte';
 	import { page as sveltePage } from '$app/stores';
-
-	debugger;
 </script>
 
-yoo
-
-<RenderServiceChat roomId={$sveltePage.params.serviceRequestId} page={$sveltePage.data.page} />
+<!-- <RenderServiceChat roomId={$sveltePage.params.serviceRequestId} page={$sveltePage.data.page} /> -->
