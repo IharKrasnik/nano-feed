fdcb0f8fc7c72727a1f22226a2ae84b2f7182201
Igor Krasnik <ihar.krasnik@gmail.com>
2024-01-08 13:14:32 +0100

    Upload and edit customers

---
+++ apps/page/src/lib/components/edit/Customer.svelte
@@ -0,0 +1,118 @@
+<script>
+	import moment from 'moment';
+	import { get, post, put, del } from 'lib/api';
+	import { fade } from 'svelte/transition';
+	import { countryCodeEmoji } from 'country-code-emoji';
+	import Button from 'lib/components/Button.svelte';
+	import FeatherIcon from 'lib/components/FeatherIcon.svelte';
+	import ToggleGroup from '$lib/components/ToggleGroup.svelte';
+	import Loader from 'lib/components/Loader.svelte';
+	import FileInput from 'lib/components/FileInput.svelte';
+
+	export let customer;
+	export let saveCustomer;
+	export let removeCustomer;
+	export let onSelected = () => {};
+</script>
+
+{#if customer._id && customer.isCollapsed}
+	<div
+		class="_section cursor-pointer"
+		on:click={() => {
+			onSelected();
+		}}
+	>
+		<div class="flex items-center">
+			{#if customer.avatarUrl}
+				<img
+					src={customer.avatarUrl || defaultAvatarUrl}
+					class="w-[30px] h-[30px] rounded-full border"
+				/>
+			{:else}
+				<div class="w-[30px] h-[30px] rounded-full border flex items-center justify-center">
+					{(customer.name || customer.email || '')[0] || ''}
+				</div>
+			{/if}
+			<div class="flex justify-between w-full items-center ml-2">
+				{#if customer.name}
+					<div class="flex items-center">
+						{customer.name}
+						<div class="ml-2 opacity-50">({customer.email})</div>
+					</div>
+				{:else}
+					{customer.email || 'anonymous'}
+				{/if}
+
+				<div class="flex gap-2 items-center">
+					{#if customer?.visitor?.payload?.deviceType}
+						{#if customer.visitor.payload.deviceType === 'desktop'}
+							<FeatherIcon theme="light" size="15" name="monitor" />
+						{:else}
+							<FeatherIcon theme="light" size="15" name="smartphone" />
+						{/if}
+					{/if}
+					{#if customer?.visitor?.geoData?.country}
+						{countryCodeEmoji(customer?.visitor?.geoData?.country)}
+					{/if}
+				</div>
+			</div>
+		</div>
+		<div class="text-sm mt-2 opacity-50">
+			{moment(customer.createdOn).format('MMM DD HH:mm')}
+		</div>
+	</div>
+{:else}
+	<div class="_section flex flex-col gap-2" in:fade={{ duration: 150 }}>
+		{#if !customer._id}
+			<div class="flex justify-between">
+				<div />
+				{#if removeCustomer}
+					<div on:click={removeCustomer}>🗑</div>
+				{/if}
+			</div>
+		{/if}
+
+		{#if !customer._id || customer.email}
+			<input class="w-full" placeholder="email@address.com" bind:value={customer.email} />
+
+			{#if !customer._id}
+				<div class="mb-2">
+					<input class="text-sm" type="checkbox" bind:checked={customer.isTrigger} /> Should Run Triggers
+				</div>
+			{/if}
+
+			<input class="w-full" placeholder="John Doe" bind:value={customer.fullName} />
+			<FileInput class="w-full" placeholder="User Avatar Url" bind:url={customer.avatarUrl} />
+		{/if}
+
+		{#if customer._id && !customer.email}
+			<div class="flex justify-between items-center">
+				Anonymous Visitor
+				<div class="flex gap-2 items-center">
+					{#if customer?.visitor?.payload?.deviceType}
+						{#if customer.visitor.payload.deviceType === 'desktop'}
+							<FeatherIcon theme="light" size="15" name="monitor" />
+						{:else}
+							<FeatherIcon theme="light" size="15" name="smartphone" />
+						{/if}
+					{/if}
+					{#if customer?.visitor?.geoData?.country}
+						{countryCodeEmoji(customer?.visitor?.geoData?.country)}
+					{/if}
+				</div>
+			</div>
+		{/if}
+
+		<input class="w-full" placeholder="Tag1, Tag2" bind:value={customer.tagsStr} />
+
+		{#if saveCustomer}
+			<Button
+				class="_small _primary mt-4 block"
+				onClick={() => {
+					debugger;
+					saveCustomer(customer);
+				}}>💾 Save Customer</Button
+			>
+		{/if}
+	</div>
+{/if}
--- apps/page/src/lib/components/edit/Customers.svelte
+++ apps/page/src/lib/components/edit/Customers.svelte
@@ -7,8 +7,13 @@
 	import FeatherIcon from 'lib/components/FeatherIcon.svelte';
 	import ToggleGroup from '$lib/components/ToggleGroup.svelte';
 	import Loader from 'lib/components/Loader.svelte';
+	import EditCustomer from '$lib/components/edit/Customer.svelte';
+	import csv from 'csvtojson';
+	import { showSuccessMessage } from 'lib/services/toast';
 
 	export let page;
+	let parentPage = page.parentPage || page;
+
 	export let selectedCustomer;
 
 	let customers = [];
@@ -23,16 +28,16 @@
 
 		if (selectedCustomerTab === 'customers') {
 			customerResult = await get(`customers`, {
-				pageId: page.parentPage?._id || page._id,
+				pageId: parentPage._id,
 				isWithEmail: true
 			});
 		} else if (selectedCustomerTab === 'visitors') {
 			customerResult = await get(`customers`, {
-				pageId: page.parentPage?._id || page._id,
+				pageId: parentPage._id,
 				isNoEmail: true
 			});
 		} else {
-			customerResult = await get(`customers`, { pageId: page.parentPage?._id || page._id });
+			customerResult = await get(`customers`, { pageId: parentPage._id });
 		}
 
 		customers = customerResult.results.map((c) => {
@@ -45,59 +50,171 @@
 	loadCustomers();
 
 	let saveCustomer = async (customer) => {
-		let updatedCustomer = await put(
-			`customers/${customer._id}?pageId=${page.parentPage?._id || page._id}`,
-			customer
-		);
+		if (customer._id) {
+			let updatedCustomer = await put(
+				`customers/${customer._id}?pageId=${parentPage._id || page._id}`,
+				customer
+			);
+			customers = customers.map((c) => {
+				if (c._id === updatedCustomer._id) {
+					updatedCustomer.isCollapsed = true;
+					return updatedCustomer;
+				}
+				return c;
+			});
+		} else {
+			let newCustomer = await post(`customers?pageId=${parentPage._id}`, customer);
+			newCustomers = newCustomers.filter((c) => c.email !== newCustomer.email);
+			newCustomer.isCollapsed = true;
 
-		customers = customers.map((c) => {
-			if (c._id === updatedCustomer._id) {
-				updatedCustomer.isCollapsed = true;
-				return updatedCustomer;
-			}
-			return c;
-		});
+			customers = [newCustomer, ...customers];
+		}
+
+		showSuccessMessage('Customer saved');
 
 		selectedCustomer = null;
 	};
+
+	let saveAllNewCustomers = async () => {
+		let createdCustomers = await post(`customers/batch?pageId=${parentPage._id}`, {
+			customers: newCustomers
+		});
+
+		showSuccessMessage(`Saved ${createdCustomers.length} customers`);
+
+		customers = [...createdCustomers.map((c) => ({ ...c, isCollapsed: true })), ...customers];
+		newCustomers = [];
+	};
+
+	let parseCSV = async (e) => {
+		const reader = new FileReader();
+
+		let parseP = new Promise((resolve, reject) => {
+			reader.readAsText(e.target.files[0]);
+
+			reader.onload = async () => {
+				const csvContent = reader.result;
+
+				let parsedFile = await csv()
+					.fromString(csvContent)
+					.preFileLine((fileLine, idx) => {
+						if (idx === 0) {
+							return fileLine.toLowerCase();
+						}
+						return fileLine;
+					});
+
+				let parsedCustomers = parsedFile.map((item) => {
+					return {
+						email: item.email,
+						fullName: item.name,
+						avatarUrl: item.avatar || item.avatarUrl,
+						tagsStr: item.tags || item.tagsStr
+					};
+				});
+
+				resolve(parsedCustomers);
+			};
+		});
+
+		return parseP;
+	};
+
+	let uploadCustomers = async (evt) => {
+		let parsedCustomers = await parseCSV(evt);
+		newCustomers = [...parsedCustomers, ...newCustomers];
+	};
+
+	let newCustomers = [];
 </script>
 
-<h3 class="text-lg font-bold mb-4">Customers</h3>
+<div class="flex justify-between items-center  mb-4">
+	<h3 class="text-lg font-bold">Customers</h3>
 
-<ToggleGroup
-	class="my-4"
-	tabs={[
-		{
-			key: 'all',
-			isSelected: selectedCustomerTab === 'all',
-			name: 'All'
-		},
-		{
-			key: 'customers',
-			isSelected: selectedCustomerTab === 'customers',
-			name: 'With Email'
-		},
+	<div class="flex items-center gap-2">
+		<div class="text-sm opacity-70 hover:opacity-100 transition cursor-pointer" on:click={() => {}}>
+			<label class="cursor-pointer block" for="csvfile">
+				<FeatherIcon size="17" name="upload" />
 
-		{
-			key: 'visitors',
-			isSelected: selectedCustomerTab === 'visitors',
-			name: 'Anonymous'
-		}
-	]}
-	onTabSelected={(tab) => {
-		selectedCustomerTab = tab.key;
-		loadCustomers();
-	}}
-/>
+				<input id="csvfile" type="file" on:change={uploadCustomers} hidden />
+			</label>
+		</div>
+		<div
+			class="text-sm opacity-70 hover:opacity-100 transition cursor-pointer"
+			on:click={() => {
+				newCustomers = [
+					{
+						email: '',
+						fullName: '',
+						avatarUrl: ''
+					},
+					...newCustomers
+				];
+			}}
+		>
+			<FeatherIcon size="17" name="plus" />
+		</div>
+	</div>
+</div>
 
-{#if isCustomersLoading}
-	<Loader />
-{:else if customers?.length}
-	{#each customers as customer}
-		{#if customer.isCollapsed}
-			<div
-				class="_section cursor-pointer"
-				on:click={() => {
+{#if newCustomers?.length}
+	<div class="mb-8">
+		Add New Customers
+		{#if newCustomers.length > 1}
+			<Button class="w-full _secondary _small mt-2" onClick={saveAllNewCustomers}
+				>Save {newCustomers.length} customers</Button
+			>
+		{/if}
+	</div>
+
+	{#each newCustomers as newCustomer}
+		<EditCustomer
+			customer={newCustomer}
+			saveCustomer={() => {
+				saveCustomer(newCustomer);
+			}}
+			removeCustomer={() => {
+				newCustomers = newCustomers.filter((c) => c !== newCustomer);
+			}}
+		/>
+	{/each}
+{:else}
+	<ToggleGroup
+		class="my-4"
+		tabs={[
+			{
+				key: 'all',
+				isSelected: selectedCustomerTab === 'all',
+				name: 'All'
+			},
+			{
+				key: 'customers',
+				isSelected: selectedCustomerTab === 'customers',
+				name: 'With Email'
+			},
+
+			{
+				key: 'visitors',
+				isSelected: selectedCustomerTab === 'visitors',
+				name: 'Anonymous'
+			}
+		]}
+		onTabSelected={(tab) => {
+			selectedCustomerTab = tab.key;
+			loadCustomers();
+		}}
+	/>
+
+	{#if isCustomersLoading}
+		<Loader />
+	{:else if customers?.length}
+		{#each customers as customer}
+			<EditCustomer
+				{customer}
+				{saveCustomer}
+				onSelected={() => {
+					selectedCustomer = customer;
+
 					customers = customers.map((c) => {
 						c.isCollapsed = true;
 						if (c._id === customer._id) {
@@ -105,79 +222,10 @@
 						}
 						return c;
 					});
-
-					selectedCustomer = customer;
 				}}
-			>
-				<div class="flex items-center">
-					{#if customer.avatarUrl}
-						<img
-							src={customer.avatarUrl || defaultAvatarUrl}
-							class="w-[30px] h-[30px] rounded-full border"
-						/>
-					{:else}
-						<div class="w-[30px] h-[30px] rounded-full border flex items-center justify-center">
-							{(customer.name || customer.email || '')[0] || ''}
-						</div>
-					{/if}
-					<div class="flex justify-between w-full items-center ml-2">
-						{#if customer.name}
-							<div class="flex items-center">
-								{customer.name}
-								<div class="ml-2 opacity-50">({customer.email})</div>
-							</div>
-						{:else}
-							{customer.email || 'anonymous'}
-						{/if}
-
-						<div class="flex gap-2 items-center">
-							{#if customer?.visitor?.payload?.deviceType}
-								{#if customer.visitor.payload.deviceType === 'desktop'}
-									<FeatherIcon theme="light" size="15" name="monitor" />
-								{:else}
-									<FeatherIcon theme="light" size="15" name="smartphone" />
-								{/if}
-							{/if}
-							{#if customer?.visitor?.geoData?.country}
-								{countryCodeEmoji(customer?.visitor?.geoData?.country)}
-							{/if}
-						</div>
-					</div>
-				</div>
-				<div class="text-sm mt-2 opacity-50">
-					{moment(customer.createdOn).format('MMM DD HH:mm')}
-				</div>
-			</div>
-		{:else}
-			<div class="_section flex flex-col gap-2" in:fade={{ duration: 150 }}>
-				{#if customer.email}
-					<input class="w-full" bind:value={customer.email} disabled />
-					<input class="w-full" placeholder="Name" bind:value={customer.name} />
-				{:else}
-					<div class="flex justify-between items-center">
-						Anonymous Visitor
-						<div class="flex gap-2 items-center">
-							{#if customer?.visitor?.payload?.deviceType}
-								{#if customer.visitor.payload.deviceType === 'desktop'}
-									<FeatherIcon theme="light" size="15" name="monitor" />
-								{:else}
-									<FeatherIcon theme="light" size="15" name="smartphone" />
-								{/if}
-							{/if}
-							{#if customer?.visitor?.geoData?.country}
-								{countryCodeEmoji(customer?.visitor?.geoData?.country)}
-							{/if}
-						</div>
-					</div>
-				{/if}
-				<textarea rows="2" class="w-full" placeholder="Notes..." bind:value={customer.notes} />
-
-				<Button class="_small _primary mt-4 block" onClick={() => saveCustomer(customer)}
-					>💾 Save Customer</Button
-				>
-			</div>
-		{/if}
-	{/each}
-{:else}
-	<div class="_section _info">You don't have any visitors yet</div>
+			/>
+		{/each}
+	{:else}
+		<div class="_section _info">You don't have any visitors yet</div>
+	{/if}
 {/if}
