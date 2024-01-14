73f51d3992050de0bcf0b1437bd85ee97f8422aa
Igor Krasnik <ihar.krasnik@gmail.com>
2024-01-14 22:51:41 +0100

    service request remove file to redeploy

--- apps/page-render/src/routes/service-requests/[serviceRequestId]/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	// import RenderServiceChat from 'lib-render/components/render/ServiceChat.svelte';
-	import { page as sveltePage } from '$app/stores';
-</script>
-
-<!-- <RenderServiceChat roomId={$sveltePage.params.serviceRequestId} page={$sveltePage.data.page} /> -->
