ee0928c51a428b78f0ddc41942f0ac32df59f963
Igor Krasnik <ihar.krasnik@gmail.com>
2024-01-16 18:27:32 +0100

    Fix app pages

--- apps/page/src/routes/app/logout/+page.svelte
+++ apps/page/src/routes/app/logout/+page.svelte
@@ -1,3 +1,10 @@
 <script>
-	debugger;
+	import currentCustomer from 'lib/stores/currentCustomer';
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+
+	if (browser) {
+		$currentCustomer = null;
+		goto('/');
+	}
 </script>
