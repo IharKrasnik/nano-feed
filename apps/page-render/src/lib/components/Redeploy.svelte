6d4ea69ea110c3173cedd4f853dc515643ae3547
Igor Krasnik <ihar.krasnik@gmail.com>
2024-05-22 19:03:56 +0200

    remove special rule for service images

--- apps/page-render/src/lib/components/Redeploy.svelte
+++ apps/page-render/src/lib/components/Redeploy.svelte
@@ -1,3 +1,3 @@
 <script>
-	// 4
+	// 1
 </script>
--- packages/lib-render/components/render/Section.svelte
+++ packages/lib-render/components/render/Section.svelte
@@ -424,9 +424,9 @@
 			{:else}
 				{#if !isSkipHeader && (section.title || section.description || section.imageUrl || section.emoji || section.interactiveRenderType)}
 					{#if section.renderType !== 'article' && section.title && (section.items?.length || section.streamSlug)}
-						<div
+						<!-- <div
 							class="absolute inset-x-0 top-20 mx-auto h-32 w-full sm:w-[650px] transform-gpu opacity-[15%] blur-[130px] bg-gradient-to-r _from-text-color _to-accent"
-						/>
+						/> -->
 					{/if}
 					<div
 						class="_section_titles relative w-full {page.theme.isTitlesLeft ||
