e5d089edaf4a5482049ba8f8b2f711de2e8cdbb9
Igor Krasnik <ihar.krasnik@gmail.com>
2024-05-29 12:28:13 +0200

    Fix featuer benefits color

--- packages/lib-render/components/render/Hero.svelte
+++ packages/lib-render/components/render/Hero.svelte
@@ -377,7 +377,7 @@
 								{#each hero.keyFeaturesStr.split('\n') as keyFeature}
 									<div class="flex items-center mb-2 shrink-0">
 										<div
-											class="mr-2 rounded-full p-1 flex"
+											class="mr-2 rounded-full p-1 flex _bg-opposite"
 											style="background-color: var(--accent-color);"
 										>
 											<Emoji color={page.theme?.buttonColor} emoji="feather:check" width={15} />
